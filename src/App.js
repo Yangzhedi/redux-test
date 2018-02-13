a321eea377e81b7e1e92d0c40f6182a4cd5bc090
杨小事er <Uiryzd@163.com>
2018-02-13 14:11:35 +0800

    feat(redux-test): add redux-test 0.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import {createStore} from 'redux'
 import {Button} from 'antd-mobile'
 
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to imooc!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          <Button type='primary'>123</Button>
-      </div>
-    );
-  }
+    render() {
+        const store = this.props.store;
+        const addCount = this.props.addCount;
+        const subCount = this.props.subCount;
+        const addCountAsync = this.props.addCountAsync;
+        const num = store.getState();
+        return (
+            <div className="App">
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <h1 className="App-title">现在有数字{num}</h1>
+                </header>
+                <Button type='primary' onClick={ () => {store.dispatch(addCount())}}> 加 </Button>
+                <Button type='primary' onClick={ () => {store.dispatch(subCount())}}> - </Button>
+                <Button type='primary' onClick={ () => {store.dispatch(addCountAsync())}}> 拖两天 </Button>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+// import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { counter, addCount, subCount, addCountAsync } from "./index.redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+const reduxDevtools = window.devToolsExtension ? window.devToolsExtension() : f => f;
+
+// applyMiddleware 管理中间件
+const store = createStore(counter, compose(
+    applyMiddleware(thunk),
+    reduxDevtools
+));
+
+function render() {
+    ReactDOM.render(<App  store={store} addCount={addCount} addCountAsync={addCountAsync} subCount={subCount}/>, document.getElementById('root'));
+}
+
+render();
+
+store.subscribe(render);
